737f4ae6f649d04bb706daf07012d46d2358f818
Oscar Umana <oscarumana1021@gmail.com>
2023-02-14 23:59:45 -0500

    first commit

---
+++ public/javascripts/data.js
@@ -0,0 +1,40 @@
+/**
+ * File name: data.js
+ * author: Oscar Umana
+ * student ID: 301247086
+ * Date: 2022-09-28
+ */
+const projects = [
+    {
+        title: "Web Development Portfolio",
+        description: "This is a web development portfolio created using Express.js and ejs views engine. This project has helped me showcase my work.",
+        imagePath: "/images/png/project1.png",
+        githubUrl:"https://github.com/oscaru21/COMP229_WEBPORTFOLIO"
+    },
+    {
+        title: "Dessert ordering ecommerce",
+        description: "Dessert ordering system developed using Spring boot for the Backend and React.js for the Frontend, capable of creating and storing new database entries and identifying users with JWTs. This is an ongoing project.",
+        imagePath: "/images/png/project5.jpg",
+        githubUrl:"https://github.com/oscaru21/dessert-ordering"
+    },
+    {
+        title: "Survey Site",
+        description: "A full-stack survey application using the MEAN stack (MongoDB, Express, Angular, Node.js). Developed as a final project for COMP229",
+        imagePath: "/images/png/project4.jpg",
+        githubUrl:"https://github.com/oscaru21/survey_site"
+    },
+    {
+        title: "Ticketing App",
+        description: "Ticketing management system developed using Node.js for the Backend and React.js for the Frontend, capable of creating and storing new database entries and identifying users with JWTs. This app has helped me a lot to keep track of my current assignments.",
+        imagePath: "/images/png/project3.png",
+        githubUrl:"https://github.com/oscaru21/support-desk"
+    },
+    {
+        title: "Library Management System",
+        description: "Library management system developed during internship at Telus International using Spring Boot for the Backend and Vue.js for the Frontend. The system helped to optimize process for a local library.",
+        imagePath: "/images/png/project2.png",
+        githubUrl:"https://github.com/oscaru21/book-borrowing-app"
+    },
+]
+
+module.exports = { projects }
